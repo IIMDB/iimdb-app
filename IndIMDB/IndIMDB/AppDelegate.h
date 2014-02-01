58ebc269ec52755a5452fe41c42ae0303bf4d59a
avhari <avhari@gmail.com>
2014-02-01 14:24:03 -0600

    adding for first time

---
+++ IndIMDB/IndIMDB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IndIMDB
+//
+//  Created by Hariharan Aiyappan vasanthakum on 2/1/14.
+//  Copyright (c) 2014 Hariharan Aiyappan vasanthakum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
