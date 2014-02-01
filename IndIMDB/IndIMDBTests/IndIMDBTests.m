2e950c703e538b09f7af14a3b986b3b4b4675343
avhari <avhari@gmail.com>
2014-02-01 14:25:09 -0600

    support files

---
+++ IndIMDB/IndIMDBTests/IndIMDBTests.m
@@ -0,0 +1,34 @@
+//
+//  IndIMDBTests.m
+//  IndIMDBTests
+//
+//  Created by Hariharan Aiyappan vasanthakum on 2/1/14.
+//  Copyright (c) 2014 Hariharan Aiyappan vasanthakum. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IndIMDBTests : XCTestCase
+
+@end
+
+@implementation IndIMDBTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
