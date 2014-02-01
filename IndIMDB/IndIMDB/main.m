2313b538cb660950275993cc896fb98cd20685e6
avhari <avhari@gmail.com>
2014-02-01 14:25:32 -0600

    project support files

---
+++ IndIMDB/IndIMDB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IndIMDB
+//
+//  Created by Hariharan Aiyappan vasanthakum on 2/1/14.
+//  Copyright (c) 2014 Hariharan Aiyappan vasanthakum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
